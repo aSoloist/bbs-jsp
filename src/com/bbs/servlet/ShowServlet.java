d12a945f33153d6b246eb5c96309f81733a9020e
Soloist <soloist1511@gmail.com>
2017-12-25 13:47:40 +0800

    删除注解

--- src/com/bbs/servlet/ShowServlet.java
+++ src/com/bbs/servlet/ShowServlet.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Created by Soloist on 2017/12/25 12:37
- */
 public class ShowServlet extends HttpServlet {
     private UserDao userDao = new UserDao();
     private StickerDao stickerDao = new StickerDao();
